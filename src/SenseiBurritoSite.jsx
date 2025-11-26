0c0461700559e9d53425cbc8a6920de65fbce137
ashleyappadoo <125403028+ashleyappadoo@users.noreply.github.com>
2025-11-26 22:02:14 +0100

    Update SenseiBurritoSite.jsx
    updat css hero image

--- src/SenseiBurritoSite.jsx
+++ src/SenseiBurritoSite.jsx
@@ -234,6 +234,16 @@ body, .sb-root {
   object-fit: cover;
   z-index: 0;
 }
+.sb-hero__image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+}
+
 
 .sb-hero__overlay {
   position: absolute;
