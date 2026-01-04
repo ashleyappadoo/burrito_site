aec5cc0b966463f467b58848489797d4bc3ac8cd
ashleyappadoo <125403028+ashleyappadoo@users.noreply.github.com>
2026-01-04 22:21:31 +0100

    Update SenseiBurritoSite.jsx
    css update

--- src/SenseiBurritoSite.jsx
+++ src/SenseiBurritoSite.jsx
@@ -233,13 +233,8 @@ body, .sb-root {
 
 }
 
-.sb-hero__video {
-  position: absolute;
-  top: 0; left: 0;
-  width: 100%; height: 100%;
-  object-fit: cover;
-  z-index: 0;
-}
+
+
 .sb-hero__image {
   position: absolute;
   top: 0;
